0f0a6c257a1c51938fb7bc451e6556d1e1a7ad04
rlnrln <rlnrln@users.noreply.github.com>
2017-07-12 16:47:38 +0200

    Support alpine's tail command.

--- dpa/start.sh
+++ dpa/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Jock! Start the engine!
 /app/dpa_${VERSION_}/startup.sh
@@ -6,5 +6,12 @@
 # Touch to update inode, just in case tomcat hasn't written to it yet.
 touch /app/dpa_${VERSION_}/iwc/tomcat/logs/catalina.out
 
+# Alpines tail command is a bit rudimentary, whereas centos and others supports --follow=name
+if [ -f /etc/alpine-release ]; then
+  TAIL_OPTS='-n 1000 -F'
+else
+  TAIL_OPTS='-n 1000 --follow=name'
+fi
+
 # Print the contents of the Tomcat catalina.out log on stdout.
-exec tail -f  /app/dpa_${VERSION_}/iwc/tomcat/logs/catalina.out
+exec /usr/bin/tail ${TAIL_OPTS} /app/dpa_${VERSION_}/iwc/tomcat/logs/catalina.out
