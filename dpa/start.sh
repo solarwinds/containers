99f7065003e53b07c06af5fa831a89c3c9f50520
rlnrln <rolf.larsson@gmail.com>
2017-08-29 19:14:39 +0200

    Revert "Merge branch 'master' into dpa-dockerfile"
    This reverts commit 8952ee97e37a17eedc1c4addc215fed389ccda5f, reversing
    changes made to f3d97c61fa2595495aa3425a0c285e42d4cc45e5.

--- dpa/start.sh
+++ dpa/start.sh
@@ -6,12 +6,5 @@
 # Touch to update inode, just in case tomcat hasn't written to it yet.
 touch /app/dpa_${VERSION_}/iwc/tomcat/logs/catalina.out
 
-# Alpines tail command is a bit rudimentary, whereas centos and others supports --follow=name
-if [ -f /etc/alpine-release ]; then
-  TAIL_OPTS='-n 1000 -F'
-else
-  TAIL_OPTS='-n 1000 --follow=name'
-fi
-
 # Print the contents of the Tomcat catalina.out log on stdout.
-exec /usr/bin/tail ${TAIL_OPTS} /app/dpa_${VERSION_}/iwc/tomcat/logs/catalina.out
+exec /usr/bin/tail -n 1000 --follow=name /app/dpa_${VERSION_}/iwc/tomcat/logs/catalina.out
