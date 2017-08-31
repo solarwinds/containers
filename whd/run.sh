9fe939c68e5485bdbdfa957dc798ef51a01ae9bc
Shabbir Kagalwala <shabbs.kagalwala@gmail.com>
2017-08-30 19:48:42 -0500

    Update run.sh
    Add command to start crond

--- whd/run.sh
+++ whd/run.sh
@@ -2,6 +2,7 @@
 echo -------------------------------------------
 echo Running Entrypoint : $EMBEDDED
 echo -------------------------------------------
+crond
 supervisord --nodaemon -c /home/docker/whd/supervisord.conf
 echo Started Supervisor 
 echo -------------------------------------------
