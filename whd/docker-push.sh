7a6e52951ca01d5022597e806fe9610055cb2044
Michael Glenn <mbg198618@gmail.com>
2021-06-26 16:23:51 -0500

    Create docker-push.sh

---
+++ whd/docker-push.sh
@@ -0,0 +1,4 @@
+docker login --username={username}
+This will prompt you to enter the docker hub account password. On successfully logging in, you will be able to push the image to the repository
+
+docker push solarwinds/dpa:latest 
